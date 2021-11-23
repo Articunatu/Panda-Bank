9a9c9a8f4a451aa631769e459e68ba71c65ffe74
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-23 10:54:32 +0100

    feat: Login class

---
+++ PandaBank/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PandaBank
+{
+    abstract class Login
+    {
+        public string userName;
+        public string password;
+    }
+}
