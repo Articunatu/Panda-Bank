c4fe5b8c8de5b758d0c5700c36205bcbba585ebd
vitoria borg vidal <vitoriaborgvidal1998@gmail.com>
2021-11-24 11:03:30 +0100

    Added a small menu to faciliate use of the program

--- PandaBank/Program.cs
+++ PandaBank/Program.cs
@@ -7,7 +7,19 @@ namespace PandaBank
         static void Main()
         {
             Console.WriteLine(" ");
-            int money = 1;
+            int money;
+            Console.WriteLine("[1] Show Accounts");
+            Console.WriteLine("[2] Tranfer Money between accounts");
+            Console.WriteLine("[3] Tranfer Money between users");
+            Console.WriteLine("[4] Create Account");
+            Console.WriteLine("[5] Deposit Money");
+            Console.WriteLine("[6] Withdraw Money");
+            Console.WriteLine("[7] Borrow Money");
+            Console.WriteLine("[7] Log out");
+            Console.Write("");
+            Int32.TryParse(Console.ReadLine(), out money);
+            Console.WriteLine();
+
             switch (money)
             {
                 case 1: Console.WriteLine(); break; 
