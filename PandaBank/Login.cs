bd029edc6615f213db84c575b615b16b48e5a2cd
vitoria borg vidal <vitoriaborgvidal1998@gmail.com>
2021-11-26 15:19:22 +0100

    Created the login fuction using a list in a method outside the main method.  That way it's easier to connect to the menu method. (Lina & Vitoria)

--- PandaBank/Login.cs
+++ PandaBank/Login.cs
@@ -4,9 +4,42 @@ using System.Text;
 
 namespace PandaBank
 {
-    abstract class Login
+    class Login
     {
-        public string userName;
-        public string password;
+        //LoginUser Ad = new LoginUser() { userName = "Admin", password = "1234" };
+        //LoginUser U1 = new LoginUser() { userName = "Hanna", password = "0000" };
+        //LoginUser U2 = new LoginUser() { userName = "Daniel", password = "1111" };
+
+
+        //public void _loginUs()
+        //{
+        //    List<LoginUser> ListUser = new List<LoginUser>();
+        //    ListUser.Add(Ad);
+        //    ListUser.Add(U1);
+        //    ListUser.Add(U2);
+
+        //    bool logintry = true;
+        //    while(logintry)
+        //    {
+        //        Console.Write("Användarnamn: ");
+        //        string userAnswer = Console.ReadLine();
+        //        Console.Write("Lösenord: ");
+        //        string userPass = Console.ReadLine();
+
+        //        LoginUser result = ListUser.Find(a => a.userName == userAnswer);
+        //        LoginUser result1 = ListUser.Find(p => p.password == userPass);
+        //        if (result == null || result1 == null)
+        //        {
+        //            Console.WriteLine("Fel användarnamn eller lösenord");
+        //        }
+        //        else
+        //        {
+        //            SignInMetod();
+        //        }
+
+        //    }
+        //}
+        
+
     }
 }
--- PandaBank/Program.cs
+++ PandaBank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PandaBank
 {
@@ -6,6 +7,15 @@ namespace PandaBank
     {
         static void Main()
         {
+            //Login L = new Login();
+            //L._loginUs();
+            LoginUs();
+
+        }
+
+        private static void SignInMetod()
+        {
+
             Console.WriteLine(" ");
             int money;
             Console.WriteLine("[1] Show Accounts");
@@ -15,15 +25,47 @@ namespace PandaBank
             Console.WriteLine("[5] Deposit Money");
             Console.WriteLine("[6] Withdraw Money");
             Console.WriteLine("[7] Borrow Money");
-            Console.WriteLine("[7] Log out");
+            Console.WriteLine("[8] Log out");
             Console.Write("");
             Int32.TryParse(Console.ReadLine(), out money);
             Console.WriteLine();
 
             switch (money)
             {
-                case 1: Console.WriteLine(); break; 
+                case 1: Console.WriteLine(); break;
                 default: break;
+            }
+        }
+        private static void LoginUs()
+        {
+            LoginUser Ad = new LoginUser() { userName = "Admin", password = "1234" };
+            LoginUser U1 = new LoginUser() { userName = "Hanna", password = "0000" };
+            LoginUser U2 = new LoginUser() { userName = "Daniel", password = "1111" };
+
+            List<LoginUser> ListUser = new List<LoginUser>();
+            ListUser.Add(Ad);
+            ListUser.Add(U1);
+            ListUser.Add(U2);
+
+            bool logintry = true;
+            while (logintry)
+            {
+                Console.Write("Användarnamn: ");
+                string userAnswer = Console.ReadLine();
+                Console.Write("Lösenord: ");
+                string userPass = Console.ReadLine();
+
+                LoginUser result = ListUser.Find(a => a.userName == userAnswer);
+                LoginUser result1 = ListUser.Find(p => p.password == userPass);
+                if (result == null || result1 == null)
+                {
+                    Console.WriteLine("Fel användarnamn eller lösenord");
+                }
+                else
+                {
+                    SignInMetod();
+                }
+
             }
         }
     }
