0ed5e4c697dd978769eb98b9d20a40d604f4d280
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-26 15:59:22 +0100

    Method for transfering money between accounts
    //Daniel & Christoffer
    
    Co-Authored-By: Daniel Thulin <91268795+danthu501@users.noreply.github.com>

--- PandaBank/Customer.cs
+++ PandaBank/Customer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PandaBank
 {
-    class Customer
+    class Customer : LoginUser
     {
         public List<Accounts> ListOfAccounts = new List<Accounts>();
 
@@ -12,12 +12,59 @@ namespace PandaBank
         {
             ListOfAccounts.Add(_Account);
         }
+
+        public Customer(List<Accounts> _ListofAccounts, string _userName, string _password)
+        {
+            userName = _userName;
+            password = _password;
+            ListOfAccounts = _ListofAccounts;
+        }
+
         public void ShoweAccounts()
         {
             foreach (var item in ListOfAccounts)
             {
                 item.PrintInfo();
             }
+        }
+
+        public void TransferAccounts()
+        {
+            Console.WriteLine("Välj ett konto att föra över pengar från:");
+            string sendAccount = Console.ReadLine();
+            Accounts account = ListOfAccounts.Find(s => s._Name == sendAccount);
+            while(account == null)
+            {
+                sendAccount = Console.ReadLine();
+            }
+
+            Console.WriteLine("Välj sen ett konto att föra över pengar till:");
+            string recieveAccount = Console.ReadLine();
+            Accounts account2 = ListOfAccounts.Find(r => r._Name == recieveAccount);
+            while (account2 == null)
+            {
+                sendAccount = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hur mycket pengar vill du föra över?");
+            int moneyamount;
+
+            try
+            {
+                moneyamount = int.Parse(Console.ReadLine());
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ogiltigt format!");
+                throw;
+            }
+
+            account._Balance -= moneyamount;
+            account2._Balance += moneyamount;
+            Console.WriteLine("Uppdaterad info:");
+            account.PrintInfo();
+            account2.PrintInfo();
         }
     }
 }
