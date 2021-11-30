78fe1c660766fed59da7eda6c74559f3eabf9754
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-30 09:32:13 +0100

    All customers are added in Admin class

--- PandaBank/Admin.cs
+++ PandaBank/Admin.cs
@@ -5,7 +5,37 @@ using System.Text;
 namespace PandaBank
 {
     class Admin : LoginUser
-    {    
+    {
+        LoginUser Ad = new Customer("Admin", "1234");
+        Customer U1 = new Customer("Hanna", "0000");
+        Customer U2 = new Customer("Daniel", "1111");
+        Customer U3 = new Customer("Emma", "2222");
+
+        Accounts a1 = new Accounts("Spar", 44000);
+        Accounts a2 = new Accounts("Lön", 22998);
+        Accounts a3 = new Accounts("Fond", 33711);
+        Accounts a4 = new Accounts("Aktie", 11000);
+        Accounts a5 = new Accounts("Privat", 5500);
+        Accounts a6 = new Accounts("Investeringar", 99999);
+
+        public void AdminSetup()
+        {
+            U1.AddAccounts(a1); U1.AddAccounts(a2); //First user's accounts
+            U2.AddAccounts(a3); U2.AddAccounts(a4);      //Second user's account
+            U3.AddAccounts(a5); U3.AddAccounts(a6);
+            ListOfCustomers.Add(U1);
+            ListOfCustomers.Add(U2);
+            ListOfCustomers.Add(U3);
+        }
+
+        public void ShowCustomers()
+        {
+            foreach (var customer in ListOfCustomers)
+            {
+                Console.WriteLine("Användare: " + customer.userName + ", Antal konton: " + customer.ListOfAccounts.Count);
+            }
+        }
+        
         public void CreateCustomer()
         {
             string nameCreated = Console.ReadLine();
