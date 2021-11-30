916b3e5cda387581a173939984d75c61e86c4cb8
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-11-30 11:42:45 +0100

    Methods for saving transactions between accounts and being able to see them

--- PandaBank/Customer2.cs
+++ PandaBank/Customer2.cs
@@ -5,6 +5,21 @@ namespace PandaBank
 {
     partial class Customer : LoginUser
     {
-        
+        List<string> Transactions = new List<string>();
+
+        public void SaveTranscation(float transaction, Accounts transferAccount, bool plusOrMinus)
+        {
+            char mathSign = plusOrMinus ? '+' : '-';
+            string savedTransfer = $"Ändrat bankkonto: {transferAccount._Name} Ändrad summa: {mathSign}{transaction}";
+            Transactions.Add(savedTransfer);
+        }
+
+        public void ShowTransactions()
+        {
+            foreach (string transaction in Transactions)
+            {
+                Console.WriteLine(transaction);
+            }
+        }
     }
 }
