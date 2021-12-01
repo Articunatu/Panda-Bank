fb0ef80c95c2a750c898aeb37b516a55e8d38d2f
danthu501 <91268795+danthu501@users.noreply.github.com>
2021-12-01 10:17:12 +0100

    Added a second Method to show transaction in both ways // Daniel o Christoffer

--- PandaBank/Customer2.cs
+++ PandaBank/Customer2.cs
@@ -13,6 +13,13 @@ namespace PandaBank
             string savedTransfer = $"Ändrat bankkonto: {transferAccount._Name} Ändrad summa: {mathSign}{transaction}";
             Transactions.Add(savedTransfer);
         }
+        public void SaveTranscation2(float transaction, Accounts transferAccount, bool plusOrMinus)
+        {
+            char mathSign = plusOrMinus ? '+' : '-';
+            string savedTransfer = $"Ändrat bankkonto: {transferAccount._Name} Ändrad summa: {mathSign}{transaction}";
+            Transactions.Add(savedTransfer);
+        }
+
 
         public void ShowTransactions()
         {
