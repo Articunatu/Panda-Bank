ff39dfe241c1c5310d9236b7c3050667500945f8
danthu501 <91268795+danthu501@users.noreply.github.com>
2021-12-02 15:56:03 +0100

    Adde Intreest

---
+++ PandaBank/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PandaBank
+{
+    public class SavingsAccount 
+    {
+        public void IntrestAmount()
+        {
+            decimal IntrestRate = 0.01M;
+            Console.Write("Skriv hur mycket vill du sätta in:");
+            decimal InsertedAmount = Convert.ToDecimal(Console.ReadLine());
+            decimal YearlyAmount = IntrestRate * InsertedAmount;
+            Console.WriteLine("Om ränta är "+IntrestRate+" kommer du att få en årlig summa på:"+ YearlyAmount);
+        }
+
+    }
+}
