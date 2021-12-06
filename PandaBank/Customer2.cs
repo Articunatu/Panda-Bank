cc87f2d3c13529783e71c5b179e59c84d20b5ace
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-12-06 13:22:17 +0100

    Methods for creating savings account, withdrawing money and deopsiting money

--- PandaBank/Customer2.cs
+++ PandaBank/Customer2.cs
@@ -21,5 +21,93 @@ namespace PandaBank
                 Console.WriteLine(transaction);
             }
         }
+
+        public void CreateSavingsAccount()
+        {
+            Console.Write("Namnge sparkonto: ");
+            string accountName = Console.ReadLine();
+            float accountAm = 0;
+            Console.WriteLine("Svenska krona: kr | US dollar: dollar | Brittisk pund: pund | Euro: euro ");
+            Console.Write("Välj valuta: ");
+            string chooseCurrency = Console.ReadLine();
+            Currency currencyEnum = (Currency)Enum.Parse(typeof(Currency), chooseCurrency);  
+            Accounts createAccounts = new Accounts(accountName, accountAm, chooseCurrency);
+            createAccounts.IsSavings = true;
+            ListOfAccounts.Add(createAccounts);
+            Console.WriteLine(createAccounts._Name + " " + createAccounts._Balance + " " + createAccounts._Currency);
+        }
+
+        public void DepositMoney()
+        {
+            Console.Write("Välj ett konto att överföra pengar till: ");
+            string depositText = Console.ReadLine();
+            Accounts depositAccount = ListOfAccounts.Find(s => s._Name == depositText);
+            while (depositAccount == null)
+            {
+                Console.Write("Ogiltigt konto! Vänligen skriv in ett nytt: ");
+                depositText = Console.ReadLine();
+                depositAccount = ListOfAccounts.Find(s => s._Name == depositText);
+            }
+
+            Console.WriteLine("Hur mycket pengar vill du föra över?");
+            float moneyAmount = 0;
+            bool isException = false;
+            do
+            {
+                try
+                {
+                    moneyAmount = float.Parse(Console.ReadLine());
+                    isException = false;
+                }
+                catch (Exception)
+                {
+                    Console.Write("Ogiltigt format! Vänligen skriv in ett nytt belopp: ");
+                    isException = true;
+                }
+            }
+            while (isException);
+
+            if (depositAccount.IsSavings == true)
+            {
+                decimal IntrestRate = 0.01M;
+                decimal YearlyAmount = IntrestRate * (decimal)moneyAmount;
+                Console.WriteLine("Om räntan är " + IntrestRate + " kommer du att få en årlig summa på:" + Math.Round(YearlyAmount, 2));
+            }
+
+            depositAccount.PrintInfo();
+        }
+
+        public void WithdrawMoney()
+        {
+            Console.Write("Välj ett konto att Ta ut pengar ifrån: ");
+            string withdrawText = Console.ReadLine();
+            Accounts withdrawAccount = ListOfAccounts.Find(s => s._Name == withdrawText);
+            while (withdrawAccount == null)
+            {
+                Console.Write("Ogiltigt konto! Vänligen skriv in ett nytt: ");
+                withdrawText = Console.ReadLine();
+                withdrawAccount = ListOfAccounts.Find(s => s._Name == withdrawText);
+            }
+
+            Console.WriteLine("Hur mycket pengar vill du ta ut?");
+            float moneyAmount = 0;
+            bool isException = false;
+            do
+            {
+                try
+                {
+                    moneyAmount = float.Parse(Console.ReadLine());
+                    isException = false;
+                }
+                catch (Exception)
+                {
+                    Console.Write("Ogiltigt format! Vänligen skriv in ett nytt belopp: ");
+                    isException = true;
+                }
+            }
+            while (isException);
+
+            withdrawAccount.PrintInfo();
+        }
     }
 }
--- PandaBank/LoginUser.cs
+++ PandaBank/LoginUser.cs
@@ -5,14 +5,13 @@ namespace PandaBank
 {
     class LoginUser
     {
-        
         public string userName { get; set; }
         public string password { get; set; }
 
-        //private string _userName;
-        //private string _password;
-        //public string UserName { get => _userName; set => _userName = value; }
-        //public string Password { get => _password; set => _password = value; }
+        private string _userName;
+        private string _password;
+        public string UserName { get => _userName; set => _userName = value; }
+        public string Password { get => _password; set => _password = value; }
 
         public List<Customer> ListOfCustomers = new List<Customer>();
     }
