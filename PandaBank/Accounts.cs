e2bb4759dce7651fc5740e0e6733775b1a0e8cd9
Christoffer Brandt <christoffer.brant@hotmail.com>
2021-12-06 13:21:48 +0100

    Option to create a savings account

--- PandaBank/Accounts.cs
+++ PandaBank/Accounts.cs
@@ -7,10 +7,12 @@ namespace PandaBank
         private string name;
         private float balance;
         private string currency;
+        private bool isSavings;
 
         public string _Name { get => name; set => name = value; }
         public float _Balance { get => balance; set => balance = value; }
         public string _Currency { get => currency; set => currency = value; }
+        public bool IsSavings { get => isSavings; set => isSavings = value; }
 
         public Accounts(string _name, float _balance, string _currency)
         {
@@ -23,6 +25,5 @@ namespace PandaBank
         {
             Console.WriteLine(name + ": "+ balance + " " + currency);
         }
-        
     }
 }
